0266de863b3ac2a50f47c31e68fbd85b3777f2d1
minjun kim <minzzzun@gmail.com>
2024-11-25 11:26:11 +0900

    [Feat] : 회전 애니메이션 추가

--- src/PokemonList.js
+++ src/PokemonList.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import './PokemonGif.css'; // CSS 파일을 별도로 작성
+import './PokemonGif.css'; // CSS 파일을 불러옴
 
 export default function PokemonList() {
   const [pokemonList, setPokemonList] = useState([]);
@@ -23,19 +23,12 @@ export default function PokemonList() {
         {pokemonList.map((pokemon, index) => (
           <div
             key={pokemon.name}
-            style={{
-              border: '1px solid #ddd',
-              borderRadius: '10px',
-              padding: '10px',
-              margin: '10px',
-              width: '150px',
-              textAlign: 'center',
-            }}
+            className="pokemon-card"
           >
             <img
               src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`}
               alt={pokemon.name}
-              style={{ width: '100px', height: '100px' }}
+              className="rotating-image" // CSS 클래스 추가
             />
             <p>{pokemon.name}</p>
           </div>
