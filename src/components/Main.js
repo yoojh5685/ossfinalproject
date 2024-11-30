e31877653a9d0eb6a36c3fa777e2009c0a5a73f6
minjun kim <minzzzun@gmail.com>
2024-11-30 11:53:31 +0900

    [Feat] : 버튼 클릭시 Survey1으로 이동하는 기능 구현

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,16 @@
 import React, { useState } from 'react';
 import '../CSS/Main.css';
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
+
 
 export default function Main() {
   const [count, setCount] = useState(0); // 서버에서 user count세서 바꾸기 
-
+  const navigate = useNavigate();
   const buttonClicked = () => {
     setCount(count + 1);
+    navigate('/survey1');
   };
 
   return (
@@ -13,7 +18,7 @@ export default function Main() {
       <h1 className="title">포켓몬이 <br /> 대학에 왔다!</h1>
       <div className="description">
         한동에 포켓몬이 출현했다고? <br />
-        나의 전공공부를 도와줄 아가를 찾아봐요
+        나의 전공공부를 도와줄 포켓몬을 찾아봐요
       </div>
       <button className="button" onClick={buttonClicked}>
         포켓몬 찾으러 가기
